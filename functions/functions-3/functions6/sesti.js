eece38de18ed2bfe853d4b366ac439827afb7e90
Ivana Markovic <ivana.markovic69@gmail.com>
2018-02-14 01:18:03 +0100

    prvideo

---
+++ functions/functions-3/functions6/sesti.js
@@ -0,0 +1,9 @@
+/*Write a function to input temperature in Centigrade and convert to Fahrenheit.*/
+function celzius(input) {
+    var rezult;
+    rezult=1.8*input+32;
+    return rezult;
+    
+}
+var farenhait=celzius(5);
+console.log(farenhait+" je stepeni farenhaita");
